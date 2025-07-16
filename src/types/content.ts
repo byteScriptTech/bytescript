31105d3aa65b492f0ac78caf14d530520c21389a
nirmalkar <hemantnirmalkar17@gmail.com>
2025-07-16 23:44:47 +0530

    hotfix: add missing types

---
+++ src/types/content.ts
@@ -0,0 +1,50 @@
+interface Challenge {
+  code: string;
+  answer: string[];
+  level?: string;
+  question?: string;
+}
+
+interface Example {
+  code: string;
+  description: string;
+  language: string;
+}
+
+interface RelatedTopic {
+  name: string;
+  description: string;
+}
+
+interface Topic {
+  name: string;
+  id: string;
+}
+
+interface BestPracticesAndCommonMistakes {
+  common_mistakes: { text: string; code?: string }[];
+  best_practices: { text: string; code?: string }[];
+}
+
+interface Introduction {
+  text: string;
+  key_concepts: string[];
+  objective: string[];
+  real_world_use_cases: string[];
+  prerequisites: string[];
+}
+
+export interface LanguageContent {
+  id: string;
+  name: string;
+  tag: string;
+  applications: string[];
+  explanation: string[];
+  best_practices_and_common_mistakes: BestPracticesAndCommonMistakes;
+  related_topics: RelatedTopic[];
+  topics: Topic[];
+  introduction: Introduction;
+  examples: Example[];
+  challenges: Challenge[];
+  [key: string]: any;
+}
